ca94b2efd59792f57df8394697906a7eeb405cea
BMatillaVaras <bmatillavaras@gmail.com>
2021-05-19 21:53:49 +0200

    delete unnecessary comments

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ async function main() {
     // Launch browser
     const browser = await chromium.launch({ headless: false, slowMo: 50 });
     const context = await browser.newContext();
-    // New page
+
     const page = await context.newPage();
-    // Navigate to: https://web.gencat.cat/ca/inici
+
     await page.goto('https://web.gencat.cat/ca/inici');
-    // To fill in the search input
+
     await page.fill('id=cercadorOcultGoogle', 'agenda cultural');
-    // To click the search button
+
     await page.click('[aria-label="Cercar"]');
 
     // await browser.close();
