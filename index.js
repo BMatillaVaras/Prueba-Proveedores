44eb2ee81f5220a0f0c00cc764b1ec3d1b1c26c1
BMatillaVaras <bmatillavaras@gmail.com>
2021-05-19 21:47:45 +0200

    add the tests requested in the test

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const { chromium } = require('playwright');
+const { chromium } = require("playwright");
 
-console.log('hola mundo');
+async function main() {
+    // Launch browser
+    const browser = await chromium.launch({ headless: false, slowMo: 50 });
+    const context = await browser.newContext();
+    // New page
+    const page = await context.newPage();
+    // Navigate to: https://web.gencat.cat/ca/inici
+    await page.goto('https://web.gencat.cat/ca/inici');
+    //
+    await page.fill('id=cercadorOcultGoogle', 'agenda cultural');
+    await page.click('[aria-label="Cercar"]');
+}
+main();
